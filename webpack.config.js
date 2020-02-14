4512eafcd86bd5b5459cdb079d721869f44fe282
Andrew Chien <aechien@gmail.com>
2020-02-14 17:44:21 -0500

    Hello world with Electron/React/Typescript

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+  {
+    entry: './src/main.ts',
+    target: 'electron-main',
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  },
+  {
+    entry: './src/app.tsx',
+    target: 'electron-renderer',
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'app.html',
+        title: 'Boardy',
+      }),
+    ],
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    output: {
+      filename: 'app.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  },
+];
